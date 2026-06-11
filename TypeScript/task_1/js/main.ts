46daa46e25b7fd4bf61425d3b6e8064e365d30b7
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-06-11 07:15:26 +0300

    Add interfaces and classes for Teacher, Directors, and StudentClass with methods

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,8 +7,45 @@ interface Teacher {
     [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
 
+interface printTeacherFunction {  (firstName: string, lastName: string): string;
+}
 
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+const student = new StudentClass("Jane", "Doe");
 const teacher3: Teacher = {
   firstName: "John",
   fullTimeEmployee: false,
