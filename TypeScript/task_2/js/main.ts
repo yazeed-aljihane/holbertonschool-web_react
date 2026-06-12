fa8c67c493d095330c5d5e13eba121b95bbf6f78
Yazeed Mustafa <yazeedgggg23@gmail.com>
2026-06-12 04:47:35 +0300

    Implement employee type checking and task execution for Director and Teacher

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -47,3 +47,16 @@ function createEmployee(salary: number | string): Teacher | Director {
     }
     return new Director();
 }
+
+
+function isDirector(employee: Teacher | Director): employee is Director {
+    return employee instanceof Director;
+}
+
+
+function executeWork(employee: Teacher | Director) :string {
+    if(isDirector(employee)){
+        return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+}
